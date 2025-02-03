411b6a97358149ebbe7d793d6c59431acd47d5bd
pavlosidukov <NoooB199505@gmail.com>
2025-02-03 11:26:37 +0200

    without refresh

--- src/components/recipe/Recipe.tsx
+++ src/components/recipe/Recipe.tsx
@@ -16,7 +16,7 @@ const Recipe: FC<PropsType> = ({recipe}) => {
         <div className={auth.loginUser ? 'recipeCard' : 'recipeCard blur'}>
             <h2 style={{cursor: 'pointer'}} onClick={() => {
                 navigate(`/recipes/details/${recipe.id}`)
-            }}>{recipe.id} -    {recipe.name}</h2>
+            }}>{recipe.id} -    {recipe.name}.</h2>
             {
                 recipe.tags.map((tag, index) => <Tag key={index} tag={tag}/>)
             }
